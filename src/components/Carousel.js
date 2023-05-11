d537745adbc31c51888caa3a8fd702b2df24e187
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-05-12 01:30:13 +0300

    carousel styling

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,28 +4,29 @@ import { Link } from "react-router-dom";
 import "swiper/swiper.min.css";
 import "swiper/swiper-bundle.css"
 
-import { Mousewheel, EffectFade } from "swiper";
+import { Mousewheel } from "swiper";
 
 export default function Carousel({ projects }) {
+
     return (
         <>
             <Swiper
                 className="projects"
                 direction={"vertical"}
-                slidesPerView={4}
-                spaceBetween={0}
+                slidesPerView={3}
+                spaceBetween={100}
+                // mousewheel={{
+                //     sensitivity: 0.5
+                // }}
                 mousewheel={true}
-                modules={[Mousewheel, EffectFade]}
+                modules={[Mousewheel]}
+
             >
                 {projects.map((project) => (
                     project.data.dynamic ?
-                        <SwiperSlide className='project' key={project.id}>
-                            {
-                                <>
-                                    <img src='https://res.cloudinary.com/dcfrq5i2y/image/upload/v1680661537/projects%20backgrounds/word_recognizer_jpf25j.png' alt="letters" />
-                                    <Link to="/projects/details" state={project} exact='true'>{project.data.name}</Link>
-                                </>
-                            }
+                        <SwiperSlide className='project' key={project.id}
+                            style={{ "--img": `url(${project.data.img})` }}>
+                            <Link to="/projects/details" state={project} exact='true'>{project.data.name}</Link>
                         </SwiperSlide>
                         : null
                 ))}
